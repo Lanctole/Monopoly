896691e6b24b868651b0b80f989c3348609e6225
Alexander Vostrikov <lanctole@yandex.ru>
2024-11-07 15:09:50 +0300

    Созданы сущности Box и Pallet

---
+++ Monopoly.Core/Entities/Box.cs
@@ -0,0 +1,34 @@
+﻿using Monopoly.Core.Interfaces;
+
+namespace Monopoly.Core.Entities;
+
+public class Box : IWarehouseObject
+{
+    public Box(double width, double height, double depth, double weight, DateOnly? expirationDate = null,
+        DateOnly? productionDate = null)
+    {
+        if (width <= 0 || height <= 0 || depth <= 0 || weight <= 0)
+            throw new ArgumentException("Размеры и вес коробки должны быть положительными числами.");
+
+        Id = Guid.NewGuid();
+        Width = width;
+        Height = height;
+        Depth = depth;
+        Weight = weight;
+        ExpirationDate = expirationDate ?? productionDate?.AddDays(100);
+        ProductionDate = productionDate;
+    }
+
+    public DateOnly? ExpirationDate { get; private set; }
+    public DateOnly? ProductionDate { get; private set; }
+    public Guid Id { get; }
+    public double Width { get; }
+    public double Height { get; }
+    public double Depth { get; }
+    public double Weight { get; }
+
+    public double CalculateVolume()
+    {
+        return Width * Height * Depth;
+    }
+}
---
+++ Monopoly.Core/Entities/Pallet.cs
@@ -0,0 +1,41 @@
+﻿using Monopoly.Core.Interfaces;
+
+namespace Monopoly.Core.Entities;
+
+public class Pallet : IWarehouseObject
+{
+    public Pallet(double width, double height, double depth)
+    {
+        Id = Guid.NewGuid();
+        Width = width;
+        Height = height;
+        Depth = depth;
+        Boxes = new List<Box>();
+    }
+
+    public DateOnly ExpirationDate => Boxes.Any()
+        ? Boxes.Min(box => box.ExpirationDate ?? DateOnly.MaxValue)
+        : DateOnly.MaxValue;
+
+    public List<Box> Boxes { get; set; }
+    public Guid Id { get; }
+    public double Width { get; }
+    public double Height { get; }
+    public double Depth { get; }
+    public double Weight => Boxes.Sum(box => box.Weight) + 30;
+
+    public double CalculateVolume()
+    {
+        var palletVolume = Width * Height * Depth;
+        var boxesVolume = Boxes.Sum(box => box.CalculateVolume());
+        return palletVolume + boxesVolume;
+    }
+
+    public void AddBox(Box box)
+    {
+        if (box.Width > Width || box.Depth > Depth)
+            throw new InvalidOperationException("Размеры коробки превышают размеры паллеты.");
+
+        Boxes.Add(box);
+    }
+}
