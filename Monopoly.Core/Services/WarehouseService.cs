70ed0b90affaae3ad022ea6c9b4ec2f996df96f0
Alexander Vostrikov <lanctole@yandex.ru>
2024-11-07 15:12:41 +0300

    Добавлен сервис сортировки и группировки данных

---
+++ Monopoly.Core/Services/WarehouseService.cs
@@ -0,0 +1,39 @@
+﻿using Monopoly.Core.Entities;
+using Monopoly.Core.Interfaces;
+
+namespace Monopoly.Core.Services;
+
+public class WarehouseService
+{
+    private readonly IDataProvider _dataProvider;
+    private List<Pallet> _pallets;
+
+    public WarehouseService(IDataProvider dataProvider, List<Pallet>? pallets = null)
+    {
+        _dataProvider = dataProvider;
+        _pallets = pallets ?? _dataProvider.GetPallets().ToList();
+    }
+
+    private void LoadPallets()
+    {
+        _pallets = _dataProvider.GetPallets().ToList();
+    }
+
+    public IEnumerable<IGrouping<DateOnly, Pallet>> GetPalletsGroupedByExpirationDate()
+    {
+        return _pallets
+            .Where(p => p.ExpirationDate != DateOnly.MaxValue)
+            .OrderBy(p => p.ExpirationDate)
+            .ThenBy(p => p.Weight)
+            .GroupBy(p => p.ExpirationDate);
+    }
+
+    public IEnumerable<Pallet> GetTopPalletsByBoxExpiration(int topCount = 3)
+    {
+        return _pallets
+            .Where(p => p.Boxes.Count != 0)
+            .OrderByDescending(p => p.Boxes.Max(b => b.ExpirationDate))
+            .ThenBy(p => p.CalculateVolume())
+            .Take(topCount);
+    }
+}
